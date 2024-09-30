362558fc5588338f91c7d6cb2433594556df5e9e
mohumadkhald <mohumadkhald@gmail.com>
2024-09-30 15:21:41 +0300

    add container name run

--- src/main/java/com/dmancloud/dinesh/demoapp/DemoAppController.java
+++ src/main/java/com/dmancloud/dinesh/demoapp/DemoAppController.java
@@ -7,15 +7,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class DemoAppController {
 
-    @GetMapping("/")
-    public String hello(Model model) {
-        model.addAttribute("message", "Hello World! This is a sample application to demonstrate an end-2-end DevOps Pipeline");
-        return "index";
-    }
+  @GetMapping("/")
+  public String hello(Model model) {
+    String containerId = System.getenv("HOSTNAME"); // or use another environment variable if needed
+    model.addAttribute("message", "Hello World! This is a sample application to demonstrate an end-2-end DevOps Pipeline");
+    model.addAttribute("containerId", containerId); // Add container ID to the model
+    return "index";
+  }
 
-    @GetMapping("/error")
-    public String handleError() {
-        return "error";
-    }
+  @GetMapping("/error")
+  public String handleError() {
+    return "error";
+  }
 }
-
